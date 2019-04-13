74efcc40fef8f9990315093d1bd31af539574504
Tomislav Kulusic <tomislav.kulusic@proteron.hr>
2019-04-13 10:10:10 +0200

    Project setup

---
+++ Api/Database/Context/SmartHousingContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartHousing.API.Database.Context
+{
+  public class SmartHousingContext : DbContext
+  {
+    // public DbSet<Application> Applications { get; set; }
+
+
+
+    public SmartHousingContext(DbContextOptions<SmartHousingContext> options) : base(options) { }
+
+   
+
+
+  }
+}
